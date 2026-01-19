447f663614f35458515819fd925d4f334a225d6e
Vireshwar313 <91110661+Vireshwar313@users.noreply.github.com>
2026-01-19 11:02:41 +0530

    commit

---
+++ Experiment-2/src/components/Home.jsx
@@ -0,0 +1,13 @@
+//export default function Home() {
+ // return <h2>Home Page</h2>;}
+function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default Home;
